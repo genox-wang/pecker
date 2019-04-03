e8bdea731765c92105e134cebb475714549691c8
wangji <genius840215@163.com>
2019-04-03 10:13:59 +0800

    修复 MODEL_NAME 宏

--- commands/model.go
+++ commands/model.go
@@ -29,7 +29,7 @@ func AddModel(name string) error {
 		"model_name":  u.SnakePath(name),
 		"model-name":  u.BarPath(name),
 		"ModelName":   u.CamelCasePath(name),
-		"MODEL_NAME":  strings.ToLower(u.SnakePath(name)),
+		"MODEL_NAME":  strings.ToUpper(u.SnakePath(name)),
 	})
 	u.Walk(templatesModelRoot, func(pathName string, info os.FileInfo, err error) error {
 		template := u.ReadFile(pathName)
